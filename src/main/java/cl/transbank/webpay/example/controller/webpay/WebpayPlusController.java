a215aae562ecdb79a7364a36da19bdef5e2b8ddd
DiegoHBustamante <99495937+DiegoHBustamante@users.noreply.github.com>
2024-06-21 12:19:52 -0400

    fix: move the logic of the if statement to the isTimeout variable

--- src/main/java/cl/transbank/webpay/example/controller/webpay/WebpayPlusController.java
+++ src/main/java/cl/transbank/webpay/example/controller/webpay/WebpayPlusController.java
@@ -73,7 +73,8 @@ public class WebpayPlusController extends BaseController {
         Map<String, Object> details = new HashMap<>();
         String buyOrder = (String) request.getSession().getAttribute("buyOrder");
         String sessionId = (String) request.getSession().getAttribute("sessionId");
-        if(request.getParameter("TBK_TOKEN")==null && tokenWs==null || request.getParameter("TBK_TOKEN").isEmpty() && tokenWs.isEmpty())
+        boolean isTimeOut = request.getParameter("TBK_TOKEN")==null && tokenWs==null || request.getParameter("TBK_TOKEN").isEmpty() && tokenWs.isEmpty();
+        if(isTimeOut)
         {
             details.put("buyOrder", buyOrder);
             details.put("sessionId", sessionId);
